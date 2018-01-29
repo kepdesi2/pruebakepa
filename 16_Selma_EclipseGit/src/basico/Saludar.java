f6e0d71f9e019bea4f13ac1ea1bccc0b10da4584
Curso mañana <Curso maana@PC13a5>
2018-01-29 10:44:21 +0100

    Mensje

--- 16_Selma_EclipseGit/src/basico/Saludar.java
+++ 16_Selma_EclipseGit/src/basico/Saludar.java
@@ -4,7 +4,7 @@ public class Saludar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola soy Selma modificado ");
+		System.out.println("Hola soy Selma modificado  ");
 	}
 
 }
